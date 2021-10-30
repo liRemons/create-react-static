22dfe4f2e43a671074fca58d9fc63f4f488a1333
Remons <remons@foxmail.com>
2021-10-30 08:43:14 +0000

    update scripts/dev.js.

--- scripts/dev.js
+++ scripts/dev.js
@@ -3,6 +3,7 @@ const { readdirSync } = require("fs");
 const pages = process.argv[2] || readdirSync("src/pages").join(',');
 if(!pages){
   console.error('未找到目录');
+  return;
 }
 const command = `webpack serve --mode=development --env pages=${pages}`;
 execSync(command, { stdio: "inherit" });
