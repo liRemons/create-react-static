8594de3a2878cb93996d52d19f558e8f09bb96ad
liRemons <remons@foxmail.com>
2021-12-30 22:52:34 +0800

    feat: 优化

--- src/apps/demo/app.jsx
+++ src/apps/demo/app.jsx
@@ -1,31 +1,7 @@
 import React from 'react';
-import { Button, Form, Card } from 'antd';
-import FormItem from '@components/Form';
-import store from './model/store';
-import { observer } from 'mobx-react';
+import List from './pages/List';
 
-@observer
-export default class App extends React.Component {
-  componentDidMount() {
-    store.getArticleList()
-  }
-  
 
-  render() {
-    const { total } = store;
-    return <Card>
-      <Form
-        labelCol={{ span: 4 }}
-        wrapperCol={{ span: 20 }}
-      >
-        <FormItem label='数字输入' component='inputNumber' value={total}></FormItem>
-        <FormItem label='输入' component='input'></FormItem>
-        <FormItem label='选择器' component='select'></FormItem>
-        <FormItem label='多选' component='checkbox'></FormItem>
-        <Form.Item>
-          <Button onClick={() => store.changePrice()}> + </Button>
-        </Form.Item>
-      </Form>
-    </Card>;
-  }
+export default function App () {
+  return <List />
 }
